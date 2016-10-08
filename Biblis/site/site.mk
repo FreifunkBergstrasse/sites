0cd0ee6eff2f94542ddea5155d9f676cb1f2df9f
Klaus Jerwan <klausjerwan@Klauss-MacBook-Pro-2.local>
2016-10-08 13:30:00 +0200

    created Kreis Bergstrasse

---
+++ Biblis/site/site.mk
@@ -0,0 +1,33 @@
+GLUON_SITE_PACKAGES := \
+	ffpb-ebtables-net-rules \
+	gluon-alfred \
+	gluon-respondd\
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-tunneldigger \
+	gluon-config-mode-hostname \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-batman-adv-15 \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-radvd \
+	gluon-status-page \
+	gluon-setup-mode \
+	haveged \
+	gluon-tunneldigger-watchdog \
+	gluon-migrate-vpn \
+	iptables \
+	iwinfo
+	 
+
+GLUON_PRIORITY ?= 3
+GLUON_LANGS ?= en de
