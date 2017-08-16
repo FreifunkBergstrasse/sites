dae8a74d475211a4c25d372cbca12c36e1ccb705
Christoph S <root@static.213-239-216-178.clients.your-server.de>
2017-08-16 08:32:35 +0200

    Domäne-03 angelegt.

---
+++ site.mk
@@ -0,0 +1,46 @@
+GLUON_SITE_PACKAGES := \
+	ffpb-ebtables-net-rules \
+	gluon-respondd\
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-tunneldigger \
+	gluon-config-mode-hostname \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-batman-adv-15 \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-radvd \
+	gluon-status-page \
+	gluon-setup-mode \
+	haveged \
+	gluon-tunneldigger-watchdog \
+	gluon-migrate-vpn \
+	iptables \
+	iwinfo \
+	gluon-ssid-changer \
+	gluon-config-mode-show-mac \
+	gluon-rfkill-disable
+ifeq ($(GLUON_TARGET),x86-generic)
+# support the usb stack on x86 devices                                                                                                                 
+     # and add a few common USB NICs                                                                                                                        
+GLUON_SITE_PACKAGES += \
+        kmod-usb-core \
+        kmod-usb2 \
+        kmod-usb-hid \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-dm9601-ether
+endif
+GLUON_PRIORITY ?= 3
+GLUON_LANGS ?= en de
+GLUON_ATH10K_MESH := ibss
+GLUON_REGION := eu
