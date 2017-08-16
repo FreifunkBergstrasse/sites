7238e11dac3f15a73e8a87f8fae785d28f4604c4
Christoph S <root@static.213-239-216-178.clients.your-server.de>
2017-08-16 08:19:49 +0200

    Domäne-02 angelegt.

--- site.mk
+++ site.mk
@@ -1,6 +1,5 @@
 GLUON_SITE_PACKAGES := \
 	ffpb-ebtables-net-rules \
-	gluon-alfred \
 	gluon-respondd\
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
@@ -26,8 +25,22 @@ GLUON_SITE_PACKAGES := \
 	gluon-tunneldigger-watchdog \
 	gluon-migrate-vpn \
 	iptables \
-	iwinfo
-	 
-
+	iwinfo \
+	gluon-ssid-changer \
+	gluon-config-mode-show-mac \
+	gluon-rfkill-disable
+ifeq ($(GLUON_TARGET),x86-generic)
+# support the usb stack on x86 devices                                                                                                                 
+     # and add a few common USB NICs                                                                                                                        
+GLUON_SITE_PACKAGES += \
+        kmod-usb-core \
+        kmod-usb2 \
+        kmod-usb-hid \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-dm9601-ether
+endif
 GLUON_PRIORITY ?= 3
 GLUON_LANGS ?= en de
+GLUON_ATH10K_MESH := ibss
+GLUON_REGION := eu
